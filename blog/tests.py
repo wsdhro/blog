94fdd72a759ff453d3a85d6cf49e1c395a70c94f
hxy <19772172@qq.com>
2020-04-22 17:52:24 +0800

    value

---
+++ blog/tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
